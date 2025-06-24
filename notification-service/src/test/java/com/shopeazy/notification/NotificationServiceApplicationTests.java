9968725639850670de0437a499539c033af30d81
Manthan <77205969+manthan-98@users.noreply.github.com>
2025-06-24 19:08:16 +0530

    Add files via upload

---
+++ notification-service/src/test/java/com/shopeazy/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopeazy.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
